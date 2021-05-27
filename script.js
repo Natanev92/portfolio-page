9c4c382f7a0cc84a44844961a2593ab007e1a5bd
Natanev92 <nvillasenor1@icloud.com>
2021-05-26 17:58:39 -0700

    making connections

---
+++ script.js
@@ -0,0 +1,10 @@
+function bottom(){
+    var myElem = document.getElementById('list')
+    console.log(myElem);
+    myElem.style.display = "none";
+}
+
+function changeText() { 
+    var myElem = document.getElementById('name')
+    myElem.style.display ="none";
+}
